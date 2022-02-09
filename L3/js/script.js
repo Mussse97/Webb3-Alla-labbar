562d43553b6463193f611158765f6cfa62b90875
Mostafa hussein <mh225qg@student.lnu.se>
2022-02-09 15:51:06 +0100

    L3 färdig

--- L3/js/script.js
+++ L3/js/script.js
@@ -5,24 +5,24 @@ var courseListElem;	// Referens till div-element där valda kurser ska läggas.
 // Initiering av globala variabler och händelsehanterare.
 function init() {
 	linkListElem = document.getElementById("linkList");
-	document.getElementById("linkBtn").addEventListener("click",listLinks);
-	
+	document.getElementById("linkBtn").addEventListener("click", listLinks);
+
 	// Array med referenser till alla li-element i den andra section
 	let courseElems = document.querySelectorAll("main section:nth-of-type(2) div:first-of-type li");
 	for (let i = 0; i < courseElems.length; i++) {
-		courseElems[i].addEventListener("click",addCourse);
+		courseElems[i].addEventListener("click", addCourse);
 		courseElems[i].style.cursor = "pointer";
 	}
 	courseListElem = document.getElementById("courseList");
-	
-	document.getElementById("teacherBtn").addEventListener("click",addTeachers); // Används i extramerit
+
+	document.getElementById("teacherBtn").addEventListener("click", addTeachers); // Används i extramerit
 } // End init
-window.addEventListener("load",init); // init aktiveras då sidan är inladdad
+window.addEventListener("load", init); // init aktiveras då sidan är inladdad
 // ---------------------------------------------------------------
 // Kopiera alla länkar ur huvudtexten och lägg upp dem i en lista.
 function listLinks() {
 	let links = document.querySelectorAll("section:nth-of-type(1) div:nth-of-type(1) a"); // Hämtar ut alla a-element
-	for (let i=0; i < links.length; i++){
+	for (let i = 0; i < links.length; i++) {
 		let copiedLinks = links[i].cloneNode(true); // kopierar alla a-element
 		let newElem = document.createElement("p");
 		linkListElem.appendChild(newElem);
@@ -35,21 +35,28 @@ function listLinks() {
 // Den kurs användaren klickat på, läggs in överst i kurslistan.
 function addCourse() {
 	let links = this.innerHTML;
+	let courses = document.querySelectorAll(" #courseList p");
+
+
+	for (i = 0; i < courses.length; i++) {
+		if (links == courses[i].innerHTML) {
+			return;
+		}
+
+
+	}
+
 	let newElem = document.createElement("p");
 	let newText = document.createTextNode(links);
 	let p = courseListElem.querySelector("p");
-	console.log(linkListElem)
+	newElem.appendChild(newText);
+	courseListElem.insertBefore(newElem, p);
+	newElem.addEventListener("click", removeCourse);
+	newElem.style.cursor = "pointer";
 
-	/*if (courseListElem.indexOf(links) !== -1){
-		return;
-	}
-	
-	else {*/
-		newElem.appendChild(newText);
-		courseListElem.insertBefore(newElem, p);
-		newElem.addEventListener("click", removeCourse);
-		newElem.style.cursor = "pointer";
-	//}
+
+
+
 } // End addCourse
 
 // Den kurs användaren klickat på i kurslistan, tas bort.
@@ -60,7 +67,7 @@ function removeCourse() {
 // ----- Extramerit -----
 // Funktion som lägger till kursansvariglärare i kurslistan
 function addTeachers() {
-	const teachers = ["Romain Herault","Rune Körnefors","Jorge Zapico"];
-	const teacherLinks = ["https://lnu.se/personal/romain.herault","http://lnu.se/personal/rune.kornefors","https://lnu.se/personal/jorgeluis.zapico/"];
-	
+	const teachers = ["Romain Herault", "Rune Körnefors", "Jorge Zapico"];
+	const teacherLinks = ["https://lnu.se/personal/romain.herault", "http://lnu.se/personal/rune.kornefors", "https://lnu.se/personal/jorgeluis.zapico/"];
+
 } // End addTeachers
